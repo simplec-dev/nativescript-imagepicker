e34a86406b84be9da093c325ab17820a72737b4d
dvnelson72 <dvnelson72@gmail.com>
2025-10-31 17:59:03 -0400

    publish files for simplec

--- publish/pack.sh
+++ publish/pack.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-set -e
-CURRENT_DIR=$(pwd)
+
 SOURCE_DIR=../src;
 TO_SOURCE_DIR=src;
 PACK_DIR=package;
@@ -17,20 +16,23 @@ pack() {
     node_modules/.bin/rimraf "$TO_SOURCE_DIR"
     node_modules/.bin/rimraf "$PACK_DIR"
 
-    # build plugin
-    echo 'Building plugin...'
+    # copy src
+    echo 'Copying src...'
     cd "$SOURCE_DIR"
-    npm run build
-    cd "$CURRENT_DIR"
-
-    # copy plugin
-    echo 'Copying plugin source...'
+    npm run tsc
+    cd ../publish
     node_modules/.bin/ncp "$SOURCE_DIR" "$TO_SOURCE_DIR"
 
     # copy README & LICENSE to src
     echo 'Copying README and LICENSE to /src...'
     node_modules/.bin/ncp "$ROOT_DIR"/LICENSE "$TO_SOURCE_DIR"/LICENSE
     node_modules/.bin/ncp "$ROOT_DIR"/README.md "$TO_SOURCE_DIR"/README.md
+
+    # compile package and copy files required by npm
+    echo 'Building /src...'
+    cd "$TO_SOURCE_DIR"
+    node_modules/.bin/tsc
+    cd ..
 
     echo 'Creating package...'
     # create package dir
@@ -42,6 +44,7 @@ pack() {
 
     # delete source directory used to create the package
     cd ..
+    echo "$TO_SOURCE_DIR"
     node_modules/.bin/rimraf "$TO_SOURCE_DIR"
 }
 
